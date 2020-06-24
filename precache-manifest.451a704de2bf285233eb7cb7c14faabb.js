e04a5ddf7d1299a33b73460d9af7440f53713588
motailab <motailab@gmail.com>
2020-06-24 22:14:16 +0600

    Updates

---
+++ precache-manifest.451a704de2bf285233eb7cb7c14faabb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e0b22795e480199a1c492089eaab448",
+    "url": "/wizard-form/index.html"
+  },
+  {
+    "revision": "4016eb3c75d2ea6009ad",
+    "url": "/wizard-form/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "fe72830d04b64451617c",
+    "url": "/wizard-form/static/css/main.4a187062.chunk.css"
+  },
+  {
+    "revision": "4016eb3c75d2ea6009ad",
+    "url": "/wizard-form/static/js/2.2964f71d.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/wizard-form/static/js/2.2964f71d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe72830d04b64451617c",
+    "url": "/wizard-form/static/js/main.c0abd60e.chunk.js"
+  },
+  {
+    "revision": "63f311c56847628efc5d",
+    "url": "/wizard-form/static/js/runtime-main.1a36967e.js"
+  },
+  {
+    "revision": "5189f5939d2f5e320d1429c127848f41",
+    "url": "/wizard-form/static/media/robo.5189f593.png"
+  }
+]);
